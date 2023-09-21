7dd5b444ec0a65dd874a6f85046c6435bba8fa97
JasonJo5 <144309692+JasonJo5@users.noreply.github.com>
2023-09-21 17:27:39 +0900

    Add files via upload

---
+++ practice&assignments3.py
@@ -0,0 +1,168 @@
+#Practice 1
+x = int(input("enter number ==> "))
+if x > 100:
+    if x < 1000:
+        print("greater than 100 and less than 1000")
+    else:
+        if x == 1000:
+            print("equal to 1000")
+        else:
+            print("greater than 1000")
+else:
+    if x == 100:
+        print("equal to 100")
+    else:
+        print("less than 100") 
+
+
+#Practice 2
+score = int(input("Enter Your Score: "))
+
+if score>= 90:
+    print("A",end="")
+else:
+    if score >= 80:
+     print("B",end="")
+    else:
+       if score >= 70:
+        print("c",end="")
+       else:
+        if score >= 60:
+         print("D",end="")
+        else:
+          print("F",end="")
+print("Grade")
+
+
+#Practice 3
+score = int(input("Enter Your Score: "))
+
+if score>= 90:
+    grade='A'
+elif score >= 80:
+    grade='B'
+elif score >= 70:
+    grade='C'
+elif score >= 60:
+    grade='D'
+else:
+   grade='F'
+     
+print("Grade is %s" % grade)
+
+prompt = "Speed of Car?\n"
+input_speed = input(prompt)
+try:
+    num_speed = int(input_speed)
+    print("Speed of Car is %s" % num_speed)
+except:
+    print("Please enter a number")
+
+
+#Practice 4
+n = 3
+while n > 0:
+    print(n)
+    n = n -1
+print("end of program")
+
+
+#Practice 5
+sum = 0
+count = 1
+n = int(input("Enter number greater than 1 : "))
+while count <= n:
+    sum = sum + count
+    count = count + 1
+
+print("sum 1 ~", n, ":", sum)
+
+
+#Practice 6
+sum = 0
+count = 1
+n = int(input("Enter number greater than 1 : "))
+while count <= n:
+    sum = sum + count
+#    count = count + 1
+
+print("sum 1 ~", n, ":", sum)
+
+
+#Practice 7
+limit = 9
+number = int(input("Display a multiplication table of number? "))
+
+for i in range(1, limit):
+    result = number * i
+    print(number, 'x', i, '=', result)
+
+
+"""\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"""
+
+# ____    _     _   _    _
+#  ||    /\    //   ||  //
+#  ||   //\\   \\   || //
+#  ||  //  \\   \\  || \\
+#  || //    \\  //  ||  \\
+
+
+#ASSIGNMENT 1
+try:
+    hours=int(input("Enter Hours: "))
+    rate=float(input("Enter Rate: "))
+except:
+   print("Error, please enter numeric input")
+
+
+if hours<=40:
+   salary = hours*rate
+   print ("your Salary is: " + str(salary))
+else:
+   if hours>=40:                                 #45
+    salary = (40*rate) + ((hours%40)*10*1.5)     #45%40 = 5x10.1,5
+    print ("your Salary is: " + str(salary))
+
+
+# ASSIGNMENT 2
+try:
+    score = int(input("Enter Your Score: "))
+except:
+  print("please enter numeric input between 0 and 100")
+
+if 0 <= score <= 100:
+ if score>= 90:
+    print("A",end="")
+ else:
+    if score >= 80:
+     print("B",end="")
+    else:
+       if score >= 70:
+        print("C",end="")
+       else:
+        if score >= 60:
+         print("D",end="")
+        else:
+          print("F",end="")
+ print(" Grade")
+
+else:
+    print("please enter numeric input between 0 and 100")
+
+
+#ASSIGNMENT 3
+total = 0
+total_input = 0
+average = 0
+
+while True:
+    number = input("Enter a number:")
+    try:
+        if number == "done":
+            break
+        total= total + float(number)
+        total_input = total_input+ 1
+        average = total / total_input
+    except:
+        print ("Invalid input")
+print ("total:", total, "Total input:", total_input, "average:", average)
