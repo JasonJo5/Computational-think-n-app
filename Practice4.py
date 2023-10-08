d364cc602db43d67ab3e686168b03ec86a7ca216
JasonJo5 <144309692+JasonJo5@users.noreply.github.com>
2023-10-09 02:08:34 +0900

    Add files via upload

--- Practice4.py
+++ Practice4.py
@@ -139,7 +139,6 @@ while True:
             if N > 0:
                 count = 0
                 for i in range(1, N + 1): 
-
                     if (i % 3 == 0):
                         count += 1 
             return count
